01ac0c860a8ff687f0762934ff42930f710ecf0c
Tofaal9152~ <tofaal91522@gmail.com>
2025-05-02 12:59:27 +0600

    add style

--- src/components/onboarding/AnnotateImage.tsx
+++ src/components/onboarding/AnnotateImage.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useEffect, useRef, useState } from "react";
 import { ReactPictureAnnotation } from "react-picture-annotation";
+import { IShapeStyle } from "react-picture-annotation/dist/types/src/Shape";
 
 const AnnotateImage = () => {
   const containerRef = useRef<HTMLDivElement>(null);
@@ -47,6 +48,7 @@ const AnnotateImage = () => {
             image="/annotateimage.jpg"
             onSelect={onSelect}
             onChange={onChange}
+            annotationStyle={defaultShapeStyle}
             width={containerSize.width}
             height={containerSize.height}
           />
@@ -67,3 +69,23 @@ const AnnotateImage = () => {
 };
 
 export default AnnotateImage;
+const defaultShapeStyle: IShapeStyle = {
+  /** text area **/
+  padding: 5, // text padding
+  fontSize: 12, // text font size
+  fontColor: "#212529", // text font color
+  fontBackground: "#f8f9fa", // text background color
+  fontFamily:
+    "-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen-Sans, Ubuntu, Cantarell, 'Helvetica Neue', Helvetica, Arial, sans-serif",
+
+  /** stroke style **/
+  lineWidth: 2, // stroke width
+  shapeBackground: "hsla(210, 16%, 93%, 0.2)", // background color in the middle of the marker
+  shapeStrokeStyle: "red", // shape stroke color
+  shadowBlur: 10, // stroke shadow blur
+  shapeShadowStyle: "hsla(210, 9%, 31%, 0.35)", // shape shadow color
+
+  /** transformer style **/
+  transformerBackground: "#5c7cfa",
+  transformerSize: 10,
+};
